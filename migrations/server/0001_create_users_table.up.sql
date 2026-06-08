e2958431121c119b3bd79cc9ccfcfafc769cc08d
Vasily Kulagin <vascool66@gmail.com>
2026-06-08 19:02:45 +0200

    moved server migrations

--- migrations/server/0001_create_users_table.up.sql
+++ migrations/server/0001_create_users_table.up.sql
@@ -1,7 +1,7 @@
 CREATE TABLE IF NOT EXISTS users (
     id         UUID PRIMARY KEY DEFAULT gen_random_uuid(),
-    login      TEXT        NOT NULL,
-    password   TEXT        NOT NULL,
+    login      VARCHAR(255) NOT NULL,
+    password   VARCHAR(128) NOT NULL,
     created_at TIMESTAMPTZ NOT NULL DEFAULT now(),
     updated_at TIMESTAMPTZ NOT NULL DEFAULT now(),
     CONSTRAINT users_login_unique UNIQUE (login)
